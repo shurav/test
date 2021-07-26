6d367ccd431188f96e1ffb4bb8a11e26cd7ec592
shurav nandy <shuravnandy@shuravs-Air.home>
2021-07-25 22:30:47 -0400

    differen commits

--- file.py
+++ file.py
@@ -2,6 +2,10 @@ print("hello github")
 print("print another line")
 print("from another file")
 print("new file")
-
 for x in range(5):
   print("ok")
+
+aList = [5, "a", 2.2]
+
+for i in aList:
+  print(2*i)
